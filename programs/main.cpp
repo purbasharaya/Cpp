55234fc56b59b1e362ca6a2182dc766438a23bd9
purbasha <puru522man@gmail.com>
2026-01-03 12:28:36 +0530

    1st program

---
+++ programs/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string> // what ever you need you can include it here.
+
+using namespace std; //it is used to avoid writing std:: before every standard library object.
+// ; is a terminator in C++ used to mark the end of a statement.
+// {} are used to define the beginning and end of a block of code, such as functions or loops.
+
+int main() {  // only one main function is allowed in a C++ program.
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+// cout- Standard output stream in C++ which is coming from the iostream library.
+
+// main function needs return type int because it returns an integer value to the operating system upon completion.
+
+// endl- It is used to insert a new line character and flush the output buffer.
+
+// main() function should return only once, typically at the end of the function.
